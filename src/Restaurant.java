7e844299bda34c7404fdcf0ef7fa14ad235812f3
Pruthu-G <pruthugambhir@gmail.com>
2025-04-26 02:05:25 +0530

    branching

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,6 @@
 public class Restaurant extends User {
     private Menu menu;
+    private double income;
     Restaurant(String name, String email, Address address, String password) {
         super(name, email, address, password);
     }
