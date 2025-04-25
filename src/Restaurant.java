d254203d953e7a9d22d95733e4b396a13d857afc
Rohan0909-bits <vasandi.rohan@gmail.com>
2025-04-26 03:11:09 +0530

    minor changea

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,19 +1,9 @@
 public class Restaurant extends User {
     private Menu menu;
-    private double income;
-    Restaurant(String name, String email, Address address, String password) {
+    pr double income;
+    public Restaurant(String name, String email, Address address, String password) {
         super(name, email, address, password);
+        this.income=0.0;
     }
-    public Menu getMenu() {
-        return menu;
-    }
-    public void setMenu(Menu menu) {
-        this.menu = menu;
-    }   
-    public double getIncome() {
-        return income;
-    }
-    public void setIncome(double income) {
-        this.income = income;
-    }
+    
 }
